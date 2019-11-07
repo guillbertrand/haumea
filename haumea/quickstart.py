4ca1af2307daf91c2359b9b0beacec1c27d5b2a2
Guillaume Bertrand <guillaume@jho.fr>
2019-11-07 10:35:22 +0100

    change wording for quickstart command

--- haumea/quickstart.py
+++ haumea/quickstart.py
@@ -16,8 +16,8 @@ _QUICKSTART_PATH = os.path.join(
 
 def haumea_parse_args():
     parser = argparse.ArgumentParser(description='Haumea Static Site Generator',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("basedir", 
-                        help="Directory name for your project ?")
+    parser.add_argument("yourprojectname", 
+                        help="What's your project name ?")
     return parser.parse_args()
 
 def main():
@@ -26,18 +26,18 @@ def main():
     logger = logging.getLogger('Haumea')
     logger.info('\U0001F680  Haumea %s - Quickstart \U0001F680' % haumea.__version__)
     args = haumea_parse_args()      
-    if(args.basedir):
+    if(args.yourprojectname):
         # create project skeleton
         try:
-            shutil.copytree(_QUICKSTART_PATH,  args.basedir)
-            logger.info('Create project folder "%s"' % args.basedir)
-            logger.info('Create content folder "%s/content/"' % args.basedir)
-            logger.info('Create content layouts "%s/layouts/"' % args.basedir)
+            shutil.copytree(_QUICKSTART_PATH,  args.yourprojectname)
+            logger.info('Create project folder "%s"' % args.yourprojectname)
+            logger.info('Create content folder "%s/content/"' % args.yourprojectname)
+            logger.info('Create content layouts "%s/layouts/"' % args.yourprojectname)
             logger.info('Add sample files...')
             logger.info('\U0001F331  Yeah ! your project is ready \U0001F331');
-            logger.info('Try to run the command "cd %s && haumea serve" to test it ! \U0001F30D' % args.basedir);
+            logger.info('Try to run the command "cd %s && haumea serve" to test it ! \U0001F30D' % args.yourprojectname);
         except:
-            logger.error('Unable to create haumea skeleton in "%s" directory' % args.basedir)
+            logger.error('Unable to create haumea skeleton in "%s" directory' % args.yourprojectname)
 
 if __name__ == '__main__':
     main()
