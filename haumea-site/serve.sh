5a91c5460f2ee8014132adde443d3d255331029c
Guillaume Bertrand <guillaume@jho.fr>
2019-11-29 16:27:51 +0100

    Fix bug on page bundle + add haumea-site sources

---
+++ haumea-site/serve.sh
@@ -0,0 +1,1 @@
+python3 -m haumea s -o ../docs -i haumea-site/
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = {
 
 setuptools.setup(
     name="haumea",
-    version="0.6.5",
+    version="0.6.6",
     author="Guillaume Betrand",
     author_email="gbe.io@pm.me",
     description="Small & fast python library to build static websites",
