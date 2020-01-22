d955ae36bc28012f70eef864c91f624f823d5cc0
Guillaume Bertrand <guillaume@jho.fr>
2020-01-22 16:18:17 +0100

    Handle ssl support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = {
 
 setuptools.setup(
     name="haumea",
-    version="0.71.2",
+    version="0.71.5",
     author="Guillaume Betrand",
     author_email="gbe.io@pm.me",
     description="Small & fast python library to build static websites",
